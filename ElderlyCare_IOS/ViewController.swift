ded427c1cb5809e998c55c531710ff64c30ce0f0
skykywind <824214765@qq.com>
2019-04-02 18:18:44 +0900

    Initial Commit

---
+++ ElderlyCare_IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ElderlyCare_IOS
+//
+//  Created by 贾富佳 on 2019/4/2.
+//  Copyright © 2019 iHolon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
